ae353717634fef31c5fcb2cf974e25030ca58eb3
Sadik-Sami <miasimanto@gmail.com>
2025-10-12 22:16:01 +0600

    added Vogus data

--- 25.Vogus_Data/main.go
+++ 25.Vogus_Data/main.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	a := 5
-	fmt.Println(a)
+	var a int8 = -128
+	var b int8 = 127
+
+	var x uint8 = 10 // unsigned (this has no sign)
+	fmt.Println(a + b)
+	fmt.Println(a, b, x)
 }
