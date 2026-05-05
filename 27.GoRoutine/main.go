e37b109b515f73c8cec7aa1b17177f33753e7333
Sadik-Sami <miasimanto@gmail.com>
2026-05-05 07:34:05 +0600

    started goroutine

---
+++ 27.GoRoutine/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var a = 10
+
+const p = 11
+
+func printHello(num int) {
+	fmt.Println("hello Sadik", num)
+}
+
+func main() {
+	go printHello(1)
+	go printHello(2)
+	go printHello(3)
+	go printHello(4)
+	go printHello(5)
+
+	fmt.Println(a, " ", p)
+
+	time.Sleep(5 * time.Second)
+}
